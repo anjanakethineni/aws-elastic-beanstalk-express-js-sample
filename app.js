9db79f4a8babe93f49d1848f20a482b78379324c
anjanakethineni <anjana.kethineni@gmail.com>
2021-02-16 13:36:36 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a trail'));
+app.get('/', (req, res) => res.send('Hola and have a wonderful day'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
